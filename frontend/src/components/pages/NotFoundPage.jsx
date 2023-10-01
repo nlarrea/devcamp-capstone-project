e016898948ef3c25e235e11d1439a7956f3589e1
nlarrea <larrea.naia@gmail.com>
2023-10-01 23:49:14 +0200

    not found page - finished

--- frontend/src/components/pages/NotFoundPage.jsx
+++ frontend/src/components/pages/NotFoundPage.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NotFoundPage = () => {
     return (
-        <div>
-            404 - Not Found
+        <div id='page-not-found-wrapper' className='container'>
+            <main>
+                <h1>Oops!</h1>
+                <h2>404 - Page Not Found</h2>
+                <p>
+                    The page you are looking for might have been removed, had its 
+                    name changed or is temporarily unavailable.
+                </p>
+                <NavLink to='/'>Go to homepage</NavLink>
+            </main>
         </div>
     );
 };
